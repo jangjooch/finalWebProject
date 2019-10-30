8085db502e4c5f3567281324fdfe775681c9a7d3
user21 <gudcjf9868@naver.com>
2019-10-30 18:15:04 +0900

    상혁이형 멘탈나감 뿌

--- src/java/web/dto/drone/DroneMissionDto.java
+++ src/java/web/dto/drone/DroneMissionDto.java
@@ -2,6 +2,8 @@ package web.dto.drone;
 
 import java.util.Date;
 
+import web.dto.member.MemberDto;
+
 public class DroneMissionDto {
 	
 	private int d_m_number;
@@ -11,8 +13,17 @@ public class DroneMissionDto {
 	private int re_num;
 	// member m_num former
 	private int m_num;
+	
+	private MemberDto memberDto;
 	private String d_m_start;
 	private Date d_m_preparation;
+	
+	public int getM_num() {
+		return m_num;
+	}
+	public void setM_num(int m_num) {
+		this.m_num = m_num;
+	}
 	public int getD_m_number() {
 		return d_m_number;
 	}
@@ -31,11 +42,12 @@ public class DroneMissionDto {
 	public void setRe_num(int re_num) {
 		this.re_num = re_num;
 	}
-	public int getM_num() {
-		return m_num;
+	
+	public MemberDto getMemberDto() {
+		return memberDto;
 	}
-	public void setM_num(int m_num) {
-		this.m_num = m_num;
+	public void setMemberDto(MemberDto memberDto) {
+		this.memberDto = memberDto;
 	}
 	public String getD_m_start() {
 		return d_m_start;
